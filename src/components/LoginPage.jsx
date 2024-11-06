2d912dd6536de8986e3c7ec9d9c065a069ee6fa8
Micheline Nshuti Manzi <nshutimanzi.mich@gmail.com>
2024-11-06 10:03:45 +0200

    Minor Updates

--- src/components/LoginPage.jsx
+++ src/components/LoginPage.jsx
@@ -1,24 +1,11 @@
 import React from "react";
 import './LoginPage.scss';
-import backgroundImage from '../assets/Login page-1~2.jpg';
+import backgroundImage from '../assets/mtn-background.jpg';
 import mtnlogo from '../assets/MTN.JO-d1d53d13.png';
-import { Button, Container, Paper, PasswordInput, TextInput, Title, Text, rem } from "@mantine/core";
-import { IconLock, IconMail } from "@tabler/icons-react";
+import { Button, Container, PasswordInput, TextInput, Title, Text } from "@mantine/core";
+import { IconLock, IconUser } from "@tabler/icons-react";
 
 const LoginPage = () => {
-    const inputStyles = {
-        input: {
-            '&:focus': {
-                borderColor: '#FFE14D'
-            }
-        }
-    };
-
-    const iconProps = {
-        size: rem(10),
-        stroke: 1,
-        color: 'gray'
-    };
     return (
         <div className="login-container">
             <div className="login-image-container">
@@ -31,35 +18,31 @@ const LoginPage = () => {
                     <div className="logo-container">
                         <img
                             src={mtnlogo}
+                            width="100px"
                             alt="MTN Logo"
-                            className="mtn-logo"
                         />
                     </div>
                 </div>
             </div>
 
             <Container className="form-container">
-                <Paper className="form-paper" radius="md">
-                    <Title order={1} className="form-title">Bulk SMS</Title>
-                    <Text size="sm" className="form-subtitle">Welcome to the Bulk SMS Platform</Text>
                     <form>
-                        <TextInput size="md" radius="lg" label="Username" placeholder="Enter your username" leftSection={<IconMail {...iconProps}/>}
-                        styles={inputStyles}
+                        <Title order={1} className="form-title">Bulk SMS</Title>
+                        <Text size="sm" opacity="0.8" className="form-subtitle">Welcome to the Bulk SMS Platform</Text>
+                        <TextInput size="md" radius="lg" label="Username" placeholder="Enter your username" leftSection={<IconUser size={25} stroke={1}/>}
                         classNames={{
                             input: 'custom-input',
                             label: 'custom-label'
                         }}/>
 
-                        <PasswordInput size="md" radius="lg" label="Password" placeholder="Enter your password" leftSection={<IconLock {...iconProps}/>}
-                        styles={inputStyles}
+                        <PasswordInput size="md" radius="lg" label="Password" placeholder="Enter your password" leftSection={<IconLock size={25} stroke={1}/>}
                         classNames={{
                             input: 'custom-input',
                             label: 'custom-label'
                         }}/>
 
-                        <Button type="submit" fullWidth className="submit-button">Login</Button>
+                        <Button type="submit" fullWidth radius="lg" className="submit-button">Login</Button>
                     </form>
-                </Paper>
             </Container>
         </div>
     );
